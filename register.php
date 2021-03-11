f275732f57c8210966d15f169553b87fc1eb754f
BIVILLE Allan <biviall@ndlp.fr>
2021-03-11 17:39:20 +0100

    ok

--- register.php
+++ register.php
@@ -31,7 +31,7 @@ if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
 }else{
 ?>
 <form class="box" action="" method="post">
-  <h1 class="box-logo box-title"><a href="https://waytolearnx.com/">WayToLearnX.com</a></h1>
+  <h1 class="box-logo box-title">APAEIA</h1>
     <h1 class="box-title">S'inscrire</h1>
   <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required />
     <input type="text" class="box-input" name="email" placeholder="Email" required />
